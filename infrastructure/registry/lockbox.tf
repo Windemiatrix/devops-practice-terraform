01b7a22e0e04fc9eb1dcd9b8beb21be9e8905735
Roman Martsev <matrixur@gmail.com>
2023-11-12 16:20:16 +0000

    fix(registry): edit lockbox secret

--- infrastructure/registry/lockbox.tf
+++ infrastructure/registry/lockbox.tf
@@ -6,11 +6,22 @@ resource "yandex_lockbox_secret" "gha" {
 resource "yandex_lockbox_secret_version" "gha" {
   secret_id = yandex_lockbox_secret.gha.id
   entries {
-    key        = "ACCESS_KEY"
-    text_value = yandex_iam_service_account_static_access_key.gha.access_key
+    key        = "YA_CR_USERNAME"
+    text_value = "json_key"
   }
   entries {
-    key        = "SECRET_KEY"
-    text_value = yandex_iam_service_account_static_access_key.gha.secret_key
+    key = "YA_CR_PASSWORD"
+    text_value = jsonencode({
+      id : yandex_iam_service_account_key.gha.id,
+      service_account_id : yandex_iam_service_account.gha.id,
+      created_at : yandex_iam_service_account_key.gha.created_at,
+      key_algorithm : yandex_iam_service_account_key.gha.key_algorithm,
+      public_key : yandex_iam_service_account_key.gha.public_key,
+      private_key : yandex_iam_service_account_key.gha.private_key
+    })
+  }
+  entries {
+    key        = "DOCKER_REGISTRY"
+    text_value = "cr.yandex"
   }
 }
