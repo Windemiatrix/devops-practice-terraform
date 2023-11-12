09973d99d7f4b9f4143e316c72b5e2dfc19a66d1
Roman Martsev <matrixur@gmail.com>
2023-11-12 15:12:02 +0000

    feat(registry): add container registry

---
+++ infrastructure/registry/provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.4.4"
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.102.0"
+    }
+  }
+}
+
+provider "yandex" {}
